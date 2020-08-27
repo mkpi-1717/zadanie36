cfe0d4c61bd5d99af256ac43339bcc535161d520
mkpi-1717 <marekpisarczyk22@gmail.com>
2020-08-27 19:30:17 +0200

    zadanie 36

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let a =21;
+let b =-1;
+
+function myFun(a, b){
+    if(a == 20 || b == 20 || a + b <= 20 ){
+        c = true;
+    } else {
+        c = false;
+    }
+    return c;
+}
+console.log(myFun(a, b));
